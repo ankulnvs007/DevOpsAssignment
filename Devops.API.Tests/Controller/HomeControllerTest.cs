37704d48e3a8cedee350c1afacdbe855434c02a8
ankulrathour <ankul.rathour@nagarro.com>
2024-04-01 22:50:17 +0530

    first commit

---
+++ Devops.API.Tests/Controller/HomeControllerTest.cs
@@ -0,0 +1,63 @@
+﻿using Devops.API.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace Devops.API.Tests.Controller
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        [Fact]
+        public void HomeController_Get_Validate_SmallerNumber_Result()
+        {
+            //AAA
+            //Arrange
+            HomeController controller = new HomeController();
+            int GuessNumber = 90;
+            string ExpectredResult = "Wrong ! Try a larger number.";
+
+            //Act
+            string result = controller.Get(GuessNumber);
+
+            //Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(ExpectredResult, result);
+        }
+        [TestMethod]
+        [Fact]
+        public void HomeController_Get_Validate_LargerNumber_Result()
+        {
+            //AAA
+            //Arrange
+            HomeController controller = new HomeController();
+            int GuessNumber = 120;
+            string ExpectredResult = "Wrong ! Try a smaller number.";
+
+            //Act
+            string result = controller.Get(GuessNumber);
+
+            //Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(ExpectredResult, result);
+        }
+        [TestMethod]
+        [Fact]
+        public void HomeController_Get_Validate_CorrectNumber_Result()
+        {
+            //AAA
+            //Arrange
+            HomeController controller = new HomeController();
+            int GuessNumber = 100;
+            string ExpectredResult = "You guessed correct number.";
+
+            //Act
+            string result=controller.Get(GuessNumber);
+
+            //Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(ExpectredResult, result);
+        }
+    }
+}
---
+++ Devops.API/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devops.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("{GuessNumber}")]
+        public string Get(int GuessNumber) 
+        {
+            if (GuessNumber < 100)
+            {
+                return "Wrong ! Try a larger number.";
+            }
+            else if (GuessNumber > 100)
+            {
+                return "Wrong ! Try a smaller number.";
+            }
+            else
+            {
+                return "You guessed correct number.";
+            }
+        }
+    }
+}
